bc0ffb407753015c98586c83f1823874c4223358
jljsj <liangjun.jiang@alipay.com>
2017-11-03 19:01:19 +0800

    Updates

---
+++ examples/parent.js
@@ -0,0 +1,197 @@
+webpackJsonp([2],{
+
+/***/ 301:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+throw new Error("Cannot find module \"rc-drawer\"");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dom__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react_dom__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_antd__ = __webpack_require__(82);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_antd_lib_style__ = __webpack_require__(86);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_antd_lib_style___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_antd_lib_style__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_antd_lib_menu_style__ = __webpack_require__(85);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_antd_lib_menu_style___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_antd_lib_menu_style__);
+throw new Error("Cannot find module \"rc-drawer/assets/index.less\"");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__assets_index_less__ = __webpack_require__(96);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__assets_index_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_7__assets_index_less__);
+/* eslint-disable no-console,react/no-multi-comp */
+
+
+
+
+
+
+
+
+
+
+
+var SubMenu = __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].SubMenu;
+var MenuItemGroup = __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].ItemGroup;
+
+// 父级的 transform 距阵必须为 none; 否则将影响 fixed;
+
+__WEBPACK_IMPORTED_MODULE_2_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+  'div',
+  null,
+  __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+    __WEBPACK_IMPORTED_MODULE_0_rc_drawer___default.a,
+    { parent: '#__react-content', width: '240px' },
+    __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+      __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */],
+      {
+        style: { width: 240 },
+        defaultSelectedKeys: ['1'],
+        defaultOpenKeys: ['sub1'],
+        mode: 'inline'
+      },
+      __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+        SubMenu,
+        {
+          key: 'sub1',
+          title: __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            'span',
+            null,
+            __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_3_antd__["b" /* Icon */], { type: 'mail' }),
+            __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+              'span',
+              null,
+              'Navigation One'
+            )
+          )
+        },
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          MenuItemGroup,
+          { key: 'g1', title: 'Item 1' },
+          __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+            { key: '1' },
+            'Option 1'
+          ),
+          __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+            { key: '2' },
+            'Option 2'
+          )
+        ),
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          MenuItemGroup,
+          { key: 'g2', title: 'Item 2' },
+          __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+            { key: '3' },
+            'Option 3'
+          ),
+          __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+            { key: '4' },
+            'Option 4'
+          )
+        )
+      ),
+      __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+        SubMenu,
+        {
+          key: 'sub2',
+          title: __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            'span',
+            null,
+            __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_3_antd__["b" /* Icon */], { type: 'appstore' }),
+            __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+              'span',
+              null,
+              'Navigation Two'
+            )
+          )
+        },
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+          { key: '5' },
+          'Option 5'
+        ),
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+          { key: '6' },
+          'Option 6'
+        ),
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          SubMenu,
+          { key: 'sub3', title: 'Submenu' },
+          __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+            { key: '7' },
+            'Option 7'
+          ),
+          __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+            { key: '8' },
+            'Option 8'
+          )
+        )
+      ),
+      __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+        SubMenu,
+        {
+          key: 'sub4',
+          title: __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+            'span',
+            null,
+            __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_3_antd__["b" /* Icon */], { type: 'setting' }),
+            __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+              'span',
+              null,
+              'Navigation Three'
+            )
+          )
+        },
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+          { key: '9' },
+          'Option 9'
+        ),
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+          { key: '10' },
+          'Option 10'
+        ),
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+          { key: '11' },
+          'Option 11'
+        ),
+        __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+          __WEBPACK_IMPORTED_MODULE_3_antd__["a" /* Menu */].Item,
+          { key: '12' },
+          'Option 12'
+        )
+      )
+    )
+  ),
+  __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
+    'div',
+    {
+      style: {
+        width: '100%', height: 667, background: '#ff0000',
+        color: '#fff', textAlign: 'center', lineHeight: '667px'
+      }
+    },
+    '\u5185\u5BB9\u533A\u5757'
+  )
+), document.getElementById('__react-content'));
+
+/***/ }),
+
+/***/ 831:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(301);
+
+
+/***/ })
+
+},[831]);
+//# sourceMappingURL=parent.js.map
