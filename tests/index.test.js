79c0628159a693d9c7b9bbc0fba397d9ebaf046a
jljsj33 <liangjun.jiang@alipay.com>
2018-06-14 16:05:51 +0800

    update test to jest

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,141 +1,81 @@
-import 'core-js/es6/map';
-import 'core-js/es6/set';
 import React from 'react';
-import ReactDom from 'react-dom';
-import TestUtils from 'react-dom/test-utils';
 import expect from 'expect.js';
+import { mount } from 'enzyme';
 import Drawer from '../src';
-import '../assets/index.less';
-import { setTimeout } from 'timers';
 
 describe('rc-drawer-menu', () => {
-  let div;
   let instance;
-
-  function createDrawerInstance(props) {
-    class DrawerDeom extends React.PureComponent {
-      constructor(childrenProps) {
-        super(childrenProps);
-        this.state = {
-          open: childrenProps.open,
-        };
-      }
-      switchMenu = () => {
-        this.setState({
-          open: !this.state.open,
-        });
-      }
-      render() {
-        return (
-          <Drawer
-            {...props}
-            open={this.state.open}
-            defaultOpen={this.props.defaultOpen}
-            level={this.props.level}
-          >
-            <div style={{ width: 200 }}>
-              test
-            </div>
-          </Drawer>
-        );
-      }
-    }
-    return ReactDom.render(<DrawerDeom {...props} />, div);
-  }
-
-  beforeEach(() => {
-    div = document.createElement('div');
-    div.className = 'react-wrapper';
-    document.body.appendChild(div);
-  });
-
-  afterEach(() => {
-    try {
-      ReactDom.unmountComponentAtNode(div);
-      document.body.removeChild(div);
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
   it('single drawer', () => {
-    instance = createDrawerInstance({});
-    const drawer = TestUtils.findRenderedDOMComponentWithClass(instance, 'drawer');
-    const drawerContent = TestUtils.findRenderedDOMComponentWithClass(instance,
-      'drawer-content-wrapper');
-    console.log(drawer);
+    instance = mount(<Drawer />);
+    const drawer = instance.find('.drawer');
+    const drawerContent = instance.find('.drawer-content-wrapper');
     expect(!!drawer).to.be(true);
-    expect(drawer.parentNode.parentNode.tagName).to.be('BODY');
-    console.log(drawerContent.style.transform);
-    expect(drawerContent.style.transform).to.be('translateX(-100%)');
+    expect(drawer.instance().parentNode.parentNode.tagName).to.be('BODY');
+    console.log('clientWidth:', drawerContent.instance().clientWidth);
+    expect(drawerContent.instance().style.transform).to.be('translateX(-100%)');
   });
 
   it('icon child is element', () => {
-    instance = createDrawerInstance({
-      handleChild: <i className="a">a</i>,
-    });
-    const icon = TestUtils.findRenderedDOMComponentWithClass(instance,
-      'drawer-handle');
-    const handleChild = icon.children[0];
-    console.log(handleChild.className);
+    instance = mount(<Drawer handleChild={<i className="a">a</i>} />);
+    const icon = instance.find('.drawer-handle');
+    const handleChild = icon.instance().children[0];
+    console.log('icon className is a: ', handleChild.className);
     expect(handleChild.className).to.be('a');
   });
 
   it('default open drawer', () => {
-    instance = createDrawerInstance({
-      defaultOpen: true,
-      level: [],
-      handleChild: (<i className="a">a</i>),
-    });
-    const drawer = TestUtils.findRenderedDOMComponentWithClass(instance,
-      'drawer-content-wrapper');
+    instance = mount(<Drawer
+      handleChild={<i className="a">a</i>}
+      defaultOpen
+      level={[]}
+    />);
+    const drawer = instance.find('.drawer-content-wrapper').instance();
+    console.log('transform is empty:', drawer.style.transform);
     expect(drawer.style.transform).to.eql('');
   });
 
-  it('switch open drawer', (done) => {
-    instance = createDrawerInstance({
-      wrapperClassName: 'drawer-2',
-    });
-    const drawer = TestUtils.findRenderedDOMComponentWithClass(instance,
-      'drawer-content-wrapper');
+  it('switch open drawer', () => {
+    instance = mount(<Drawer />);
+    jest.useFakeTimers();
+    const drawer = instance.find('.drawer-content-wrapper').instance();
+
     console.log('第一次百分比：', drawer.style.transform);
     expect(drawer.style.transform).to.be('translateX(-100%)');
-    instance.switchMenu();
-    setTimeout(() => {
-      console.log(drawer.style.transform);
-      expect(drawer.style.transform).to.eql('');
-      instance.switchMenu();
-      setTimeout(() => {
-        console.log('第二次变绝对值：', drawer.style.transform);
-        expect(drawer.style.transform).to.be('translateX(-200px)');
-        done();
-      }, 500);
-    }, 500);
+    instance.setProps({
+      open: true,
+    });
+    jest.runAllTimers();
+    console.log(drawer.style.transform);
+    expect(drawer.style.transform).to.eql('');
+    instance.setProps({
+      open: false,
+    });
+    console.log('第二次变绝对值：', drawer.style.transform);
+    jest.runAllTimers();
+    expect(drawer.style.transform).to.be('translateX(-0px)');
+    jest.useRealTimers();
   });
   it('getContainer is null', () => {
-    instance = createDrawerInstance({
-      getContainer: null,
-    });
-    const drawer = TestUtils.findRenderedDOMComponentWithClass(instance, 'drawer');
+    instance = mount(<div className="react-wrapper">
+      <Drawer getContainer={null} wrapperClassName="drawer-wrapper"/>
+    </div>);
+    const drawer = instance.find('.drawer').instance();
     console.log(drawer.parentNode.parentNode.className);
+    expect(drawer.parentNode.className).to.be('drawer-wrapper');
     expect(drawer.parentNode.parentNode.className).to.be('react-wrapper');
   });
-  it('click open close', (done) => {
-    instance = createDrawerInstance({});
-    const content = TestUtils.findRenderedDOMComponentWithClass(instance,
-      'drawer-content-wrapper');
+  it('click open close', () => {
+    instance = mount(<Drawer />);
+    const content = instance.find('.drawer-content-wrapper').instance();
     console.log(content.style.transform);
     expect(content.style.transform).to.be('translateX(-100%)');
-    const handle = TestUtils.findRenderedDOMComponentWithClass(instance, 'drawer-handle');
-    TestUtils.Simulate.click(handle);
+    const handle = instance.find('.drawer-handle');
+    handle.simulate('click');
     console.log(content.style.transform);
     expect(content.style.transform).to.be('');
-    setTimeout(() => {
-      const mask = TestUtils.findRenderedDOMComponentWithClass(instance, 'drawer-mask');
-      TestUtils.Simulate.click(mask);
-      console.log(content.style.transform);
-      expect(content.style.transform).to.be('translateX(-200px)');
-      done();
-    }, 450);
+    const mask = instance.find('.drawer-mask');
+    mask.simulate('click');
+    console.log(content.style.transform);
+    expect(content.style.transform).to.be('translateX(-0px)');
   });
 });
