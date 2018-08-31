afbf1f79c7706ffc5c0a414c266f3cdc7c8b82a2
jljsj33 <liangjun.jiang@alipay.com>
2018-08-31 17:51:22 +0800

    re simple

--- examples/simple.js
+++ examples/simple.js
@@ -15,7 +15,7 @@ const MenuItemGroup = Menu.ItemGroup;
 
 ReactDom.render((
   <div >
-    <Drawer width="20vw" placement="right" level={null}>
+    <Drawer width="20vw">
       <Menu
         style={{ height: '200%' }}
         defaultSelectedKeys={['1']}
