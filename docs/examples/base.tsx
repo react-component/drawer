0dc6050f92dba12b08414ecc8c11aee41ca1dc94
黄建峰 <645381995@qq.com>
2022-05-07 21:30:00 +0800

    feat: init

---
+++ docs/examples/base.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import Drawer from 'rc-drawer';
+
+const Child = () => {
+  useEffect(() => {
+    console.log('load');
+  }, []);
+
+  return <>child</>;
+};
+
+const Demo = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <>
+      <button onClick={() => setVisible(true)}>打开</button>
+      <Drawer
+        // onChange={open => {
+        //   console.log('open', open);
+        // }}
+        open={visible}
+        onClose={() => setVisible(false)}
+        handler={false}
+        level={null}
+        // afterVisibleChange={(c: boolean) => {
+        //   console.log('transitionEnd: ', c);
+        // }}
+        placement="right"
+        width={300}
+      >
+        <Child />
+      </Drawer>
+    </>
+  );
+};
+
+export default Demo;
