1d24b0f8f6c8e5ebd7515e4a9a740d2cecb695d9
黄建峰 <645381995@qq.com>
2022-05-08 10:29:49 +0800

    feat: test

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //   collectCoverage: true,
+  // coverageReporters: ['clover', 'json', 'lcov', 'text', 'cobertura'],
+  // setupFiles: ['./setupTests.js'],
+  //   coveragePathIgnorePatterns: ['/node_modules/', 'src/.umi', 'demo'],
+  //   modulePathIgnorePatterns: ['/docs/', '/__snapshots__/'],
+};
--- tests/drawer.spec.tsx
+++ tests/drawer.spec.tsx
@@ -1,92 +1,51 @@
-/* eslint-disable max-classes-per-file */
-// eslint-disable react/no-multi-comp
-import { mount } from 'enzyme';
-import * as React from 'react';
+import React, { useRef } from 'react';
 import Drawer from '../src/';
 import type { IDrawerProps } from '../src/IDrawerPropTypes';
-import toJson from 'enzyme-to-json';
+import { cleanup, render } from '@testing-library/react';
 
-class DrawerTesterRef extends React.Component {
-  public container: HTMLDivElement;
-  public getContainer = () => {
-    return this.container;
-  };
-  public saveContainer = (container: HTMLDivElement) => {
-    this.container = container;
-  };
-  public render() {
-    return (
-      <div>
-        <div ref={this.saveContainer} className="main" />
-        <Drawer {...this.props} open={true} getContainer={this.getContainer}>
+const DrawerTesterRef = (props: IDrawerProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  return (
+    <div>
+      <div ref={containerRef} className="main" />
+      <Drawer getContainer={() => containerRef.current} {...props}>
+        <p className="text">Here is content of Drawer</p>
+      </Drawer>
+    </div>
+  );
+};
+
+const DrawerTesterDom = (props: IDrawerProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  return (
+    <div>
+      <div ref={containerRef} className="main" />
+      {open && (
+        <Drawer getContainer={() => containerRef.current} {...props}>
           <p className="text">Here is content of Drawer</p>
         </Drawer>
-      </div>
-    );
-  }
-}
-/* eslint react/no-multi-comp: 0 */
-
-interface IState {
-  visible: boolean;
-}
-// tslint:disable-next-line:max-classes-per-file
-class DrawerTesterDom extends React.Component<IDrawerProps, IState> {
-  public container: HTMLDivElement;
-  constructor(props: IDrawerProps) {
-    super(props);
-    this.state = { visible: false };
-  }
-  public componentDidMount() {
-    this.setState({ visible: true }); // eslint-disable-line react/no-did-mount-set-state
-  }
-  public getContainer = () => {
-    return this.container;
-  };
-  public saveContainer = (container: HTMLDivElement) => {
-    this.container = container;
-  };
-  public render() {
-    return (
-      <div>
-        <div ref={this.saveContainer} className="main" />
-        {this.state.visible ? (
-          <Drawer
-            {...this.props}
-            open={true}
-            getContainer={this.getContainer()}
-          >
-            <p className="text">Here is content of Drawer</p>
-          </Drawer>
-        ) : null}
-      </div>
-    );
-  }
-}
-
-/* eslint react/no-multi-comp: 0 */
-// tslint:disable-next-line:max-classes-per-file
-const DrawerTesterBoolean = props => (
-  <div>
-    <Drawer {...props} open={true} getContainer={false}>
-      <p className="text">Here is content of Drawer</p>
-    </Drawer>
-  </div>
-);
+      )}
+    </div>
+  );
+};
 
 describe('Drawer', () => {
+  afterEach(() => {
+    cleanup();
+  });
   it('render function', () => {
-    const wrapper = mount(<DrawerTesterRef />);
-    expect(toJson(wrapper.render())).toMatchSnapshot();
+    render(<DrawerTesterRef open />);
+    expect(document.body).toMatchSnapshot();
   });
 
   it('render dom', () => {
-    const wrapper = mount(<DrawerTesterDom />);
-    expect(toJson(wrapper.render())).toMatchSnapshot();
+    const { rerender } = render(<DrawerTesterDom />);
+    rerender(<DrawerTesterDom open />);
+    expect(document.body).toMatchSnapshot();
   });
 
   it('render boolean', () => {
-    const wrapper = mount(<DrawerTesterBoolean />);
-    expect(toJson(wrapper.render())).toMatchSnapshot();
+    render(<DrawerTesterRef open getContainer={false} />);
+    expect(document.body).toMatchSnapshot();
   });
 });
