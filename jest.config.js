0e4123ae33c3b52667af771cecc89074f31438ef
黄建峰 <645381995@qq.com>
2022-05-08 10:32:57 +0800

    feat: test

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  //   collectCoverage: true,
-  // coverageReporters: ['clover', 'json', 'lcov', 'text', 'cobertura'],
-  // setupFiles: ['./setupTests.js'],
-  //   coveragePathIgnorePatterns: ['/node_modules/', 'src/.umi', 'demo'],
-  //   modulePathIgnorePatterns: ['/docs/', '/__snapshots__/'],
-};
