32728635845a37e168b5594f7064717f4111c472
二货机器人 <smith3816@gmail.com>
2022-12-27 10:59:06 +0800

    fix: onClose only trigger when open

--- tests/motion.spec.tsx
+++ tests/motion.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import Drawer from '../src';
 
@@ -35,5 +35,42 @@ describe('motion', () => {
     expect(container.querySelector('.rc-drawer-content-wrapper')).toHaveStyle({
       background: 'red',
     });
+  });
+
+  it('mask close should only trigger once', () => {
+    let closeCount = 0;
+
+    const Demo = () => {
+      const [open, setOpen] = React.useState(true);
+      const onClose = () => {
+        closeCount += 1;
+        setOpen(false);
+      };
+
+      const sharedMotion = {
+        motionName: 'bamboo',
+        motionAppear: false,
+        motionEnter: false,
+        motionLeave: true,
+      };
+
+      return (
+        <Drawer
+          motion={sharedMotion}
+          maskMotion={sharedMotion}
+          open={open}
+          onClose={onClose}
+          getContainer={false}
+        />
+      );
+    };
+
+    const { container } = render(<Demo />);
+
+    for (let i = 0; i < 10; i += 1) {
+      fireEvent.click(container.querySelector('.rc-drawer-mask'));
+    }
+
+    expect(closeCount).toBe(1);
   });
 });
