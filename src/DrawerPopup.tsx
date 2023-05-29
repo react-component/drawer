3ec3caf3b3859feb707a29f5792b1072240cd1aa
Robert Kuykendall <robert@rkuykendall.com>
2023-05-29 02:53:44 -0400

    feat: support data-* (#404)
    * feat: support data-*
    
    Used https://github.com/react-component/dialog/pull/259as reference.
    
    * Add test

--- src/DrawerPopup.tsx
+++ src/DrawerPopup.tsx
@@ -6,6 +6,7 @@ import DrawerPanel from './DrawerPanel';
 import DrawerContext from './context';
 import type { DrawerContextProps } from './context';
 import KeyCode from 'rc-util/lib/KeyCode';
+import pickAttrs from 'rc-util/lib/pickAttrs';
 import { parseWidthHeight } from './util';
 
 const sentinelStyle: React.CSSProperties = {
@@ -273,6 +274,7 @@ function DrawerPopup(props: DrawerPopupProps, ref: React.Ref<HTMLDivElement>) {
               ...motionStyle,
               ...contentWrapperStyle,
             }}
+            {...pickAttrs(props, { data: true })}
           >
             <DrawerPanel
               containerRef={motionRef}
--- tests/index.spec.tsx
+++ tests/index.spec.tsx
@@ -359,4 +359,11 @@ describe('rc-drawer-menu', () => {
     );
     errSpy.mockRestore();
   });
+
+  it('pass data props to internal div', () => {
+    const value = 'bamboo';
+    const { unmount } = render(<Drawer open data-attr={value} />);
+    expect(document.querySelector('.rc-drawer-content-wrapper')).toHaveAttribute('data-attr',value);
+    unmount();
+  });
 });
